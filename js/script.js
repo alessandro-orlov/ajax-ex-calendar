39c70c9b243d445647fcd0ac04007f6f11022055
Alessandro <alex.ixs@hotmail.com>
2020-06-29 16:27:10 +0200

    stampo a schermo ogni giorno del mese

--- js/script.js
+++ js/script.js
@@ -2,34 +2,46 @@ $(document).ready(function(){
 
 
   // MOMENT
-  var date = moment('2018-06-29');
-  console.log(date);
+  var gennaio = moment('2018-01').format('MMMM YYYY')
+  $('.wrapper h1').text(gennaio)
 
-  var giorno = date._i
-  console.log(giorno);
+  var giorniNelMese = moment("2018-01", "YYYY-MM").daysInMonth();
 
-  var mese = moment().month()
 
-  // Stampo i giorni del mese
-  for (var i = 0; i < 1; i++) {
-    dayTamplate(mese);
-  }
+  dayTamplate(giorniNelMese)
+
 
 
   // =====================================================
   // =============== FUNCTIONS ===========================
 
   // Handlebars tamplate function
-  function dayTamplate(day) {
-    var source = $('#calendar-template').html();
-    var template = Handlebars.compile(source);
+  function dayTamplate(daysInMonth) {
+    // Oggetto della data
+    var data = moment('2018-01-01')
+    console.log(data)
 
-    var context = {day};
-    var html = template(context);
+    // Converto la data(perche oggetto) in stringa
+    var dataConvertita = data.format('D MMMM')
 
-    // append tamplate to all'elemnto in HTML
-    $('.days').append(html)
+    // CICLO per stampare tutti i giorni del mese
+    for (var i = 0; i < giorniNelMese; i++) {
+      // hendlebars template
+      var source = $('#calendar-template').html();
+      var template = Handlebars.compile(source);
+
+      var context = {dataConvertita}
+      var html = template(context);
+
+      // Inserisco elemento del template nel html
+      $('.calendar').append(html);
+
+      data.add(1, 'd');
+      dataConvertita = data.format('D MMMM');
+    }
+
   }
+
 
 
 }); // End document ready
