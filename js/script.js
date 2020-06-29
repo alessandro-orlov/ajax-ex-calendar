acf32d0a3ad85dec0aec34545a78b410b8d0c955
Alessandro <alex.ixs@hotmail.com>
2020-06-29 17:05:49 +0200

    Fatto chiamata ajax

--- js/script.js
+++ js/script.js
@@ -11,36 +11,63 @@ $(document).ready(function(){
   dayTamplate(giorniNelMese)
 
 
-
   // =====================================================
   // =============== FUNCTIONS ===========================
 
   // Handlebars tamplate function
   function dayTamplate(daysInMonth) {
-    // Oggetto della data
-    var data = moment('2018-01-01')
-    console.log(data)
 
-    // Converto la data(perche oggetto) in stringa
-    var dataConvertita = data.format('D MMMM')
+      // Ajax call
+      $.ajax(
+        {
+          url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+          method: "GET",
+          success: function(info) {
 
-    // CICLO per stampare tutti i giorni del mese
-    for (var i = 0; i < giorniNelMese; i++) {
-      // hendlebars template
-      var source = $('#calendar-template').html();
-      var template = Handlebars.compile(source);
+            var giornoFestivo = info['response']
 
-      var context = {dataConvertita}
-      var html = template(context);
+            for(key in giornoFestivo) {
 
-      // Inserisco elemento del template nel html
-      $('.calendar').append(html);
+              var holydays = giornoFestivo[key]
+              console.log(holydays['date'])
 
-      data.add(1, 'd');
-      dataConvertita = data.format('D MMMM');
-    }
+            }
 
-  }
+            // Oggetto della data
+            var data = moment('2018-01-01')
+            var corispondenza = data['_i']
+            console.log(corispondenza)
+
+            // Converto la data(perche oggetto) in stringa
+            var dataConvertita = data.format('D MMMM')
+
+            // CICLO per stampare tutti i giorni del mese
+            for (var i = 0; i < daysInMonth; i++) {
+              // hendlebars template
+              var source = $('#calendar-template').html();
+              var template = Handlebars.compile(source);
+
+              var context = {dataConvertita}
+              var html = template(context);
+
+              // Inserisco elemento del template nel html
+              $('.calendar').append(html);
+
+              data.add(1, 'd');
+              dataConvertita = data.format('D MMMM');
+
+            } // End ciclo for
+
+          },
+          error: function(request, state, error) {
+
+          }
+        }
+      ); // END Ajax call
+
+
+
+  } // End Function
 
 
 
