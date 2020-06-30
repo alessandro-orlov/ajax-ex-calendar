e3e9a982f6d649aea629030d8e4ba44ed8dddc4d
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-06-30 08:10:15 +0200

    Altre prove

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+var mese = 0
 $.ajax(
   {
-    url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+    url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=" + mese,
     method: "GET",
     success: function(info) {
       var festivita = info['response']
-      console.log(festivita)
-
+      var feste = [];
+      for (var i = 0; i < festivita.length; i++) {
+        feste[i] = festivita[i]['date'];
+      }
+  
       // Mese iniziale Gennaio
       var gennaio = moment('2018-01').format('MMMM YYYY');
       $('.wrapper h1').text(gennaio)
@@ -22,22 +25,25 @@ $.ajax(
 
       // Stampo tutti i giorni del mese
       for (var i = 0; i < giorniNelMese; i++) {
+
         // Giorno successivo
         var nextDay = moment(date).add(i, 'd');
 
         // Converto il giorno succesiovo nel formato desiderato
         var dayMonth = nextDay.format('D MMMM');
-        console.log('giorno del mese ' + dayMonth);
-
+        // console.log('giorno del mese ' + dayMonth);
 
         $.each(festivita, function(key, value ) {
+
+          var infoFestivita = festivita[key];
+          // console.log(infoFestivita);
 
           var dataFestivo = value['date'];
           var nomeFestivo = value['name'];
           var dataConfronto = moment(dataFestivo).format('D MMMM');
 
           var singolaFestivita = nomeFestivo + ' ' + dataConfronto;
-          console.log('Each singola festivita ' + singolaFestivita);
+          // console.log('Each singola festivita ' + singolaFestivita);
 
           if(singolaFestivita.includes(dayMonth) ) {
             var giornoDelMese = dataConfronto + ' ' + nomeFestivo;
